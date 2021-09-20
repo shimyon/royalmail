6bf0abea8ad521834413fa2b63f6fc5937329d32
shimyon <shimyon@hotmail.com>
2021-09-20 16:44:24 +0530

    Change map type

--- index.php
+++ index.php
@@ -164,15 +164,30 @@
     var googleAddress = "northumerland";
 
     function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: {
-          lat: -34.397,
-          lng: 150.644
+      // var map = new google.maps.Map(document.getElementById('map'), {
+      //   zoom: 8,
+      //   center: {
+      //     lat: -34.397,
+      //     lng: 150.644
+      //   }
+      // });
+      var map = new google.maps.StreetViewPanorama(
+        document.getElementById("map"), {
+          position: {
+            lat: 42.345573,
+            lng: -71.098326
+          },
+          addressControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM_CENTER,
+          },
+          linksControl: false,
+          panControl: false,
+          enableCloseButton: false,
         }
-      });
+      );
       geocoder = new google.maps.Geocoder();
       codeAddress(geocoder, map);
+
     }
 
     addressNow.listen('load', function(control) {
@@ -276,11 +291,20 @@
         'address': googleAddress
       }, function(results, status) {
         if (status === 'OK') {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-          });
+          if (results.length > 0) {
+            let loc = results[0].geometry.location;
+            astorPlace = {
+              lat: loc.lat(),
+              lng: loc.lng()
+            };
+            map.setPosition(astorPlace);
+
+            // map.setCenter(results[0].geometry.location);
+            // var marker = new google.maps.Marker({
+            //   map: map,
+            //   position: results[0].geometry.location
+            // });
+          }
         } else {
           alert('Geocode was not successful for the following reason: ' + status);
         }
